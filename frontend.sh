de3d4662fe3ffe0289988b44481b778ba8603efb
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 10:41:58 +0200

    Frontend Add Headings

--- frontend.sh
+++ frontend.sh
@@ -12,7 +12,8 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-fro
 cd /usr/share/nginx/html
 
 
-echo EXTRACT CONTENTunzip /tmp/frontend.zip
+echo EXTRACT CONTENT
+unzip /tmp/frontend.zip
 
 echo RESTART SERVICE
 systemctl enable nginx
