0a389c9edaed2da2c3a06babce345debb0593dbc
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 11:11:09 +0200

    variable demo

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,7 +1,7 @@
 # If we hold some data with some name t is variable
 # If we assign a name to a set of data then it is called as a variable
 
-
+# User defined variables
 # syntax: var = data
 
 x=10
@@ -10,3 +10,14 @@ x=10
 
 
 echo x value is $x
+
+#System Special Variables
+
+#$0, $1, ...., $n
+#$#, S*, $?
+
+
+# $? - Exit status of a command
+
+ls
+echo exit status - $?
