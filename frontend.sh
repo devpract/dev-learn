72e5d22ffa45ef4da759109c0e82910adf85a8c9
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 11:13:56 +0200

    Exit status in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,27 @@
 echo -e "\e[31mINSTALLING NGINX\e[0m"
 dnf install nginx -y
+echo exit status- $?
 
 echo -e "\e[31mCOPY EXPENSE CONFIG FILE\e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo exit status- $?
 
 echo -e "\e[31mCLEAN OLD CONTENT\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo exit status- $?
 
 echo -e "\e[31mDOWNLOAD FRONTEND CONTENT\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+echo exit status- $?
 cd /usr/share/nginx/html
 
 
 echo -e "\e[31mEXTRACT CONTENT\e[0m"
 unzip /tmp/frontend.zip
+echo exit status- $?
 
 echo -e "\e[31mRESTART SERVICE\e[0m"
 systemctl enable nginx
 systemctl restart nginx
+echo exit status- $?
 
