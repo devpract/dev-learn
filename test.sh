a81f9c5359d641a7e961d4574deb6f14d45ced95
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 10:48:10 +0200

    Adding colors test

--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
 echo Hello Worlds
 echo Hallo
+
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
