312ab7f44edf905088d7191e43a36d1814944833
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 11:06:02 +0200

    Adding colors in frontend

---
+++ 03-variable.sh
@@ -0,0 +1,12 @@
+# If we hold some data with some name t is variable
+# If we assign a name to a set of data then it is called as a variable
+
+
+# syntax: var = data
+
+x=10
+
+# Acces: $var or ${var}
+
+
+echo x value is $x
