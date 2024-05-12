6d320675e736e0ca36fae92f8967fb1096a05498
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 10:51:33 +0200

    Adding colors in frontend

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
-echo INSTALLING NGINX
+echo -e "\e[33mINSTALLING NGINX\e[0m"
 dnf install nginx -y
 
-echo COPY EXPENSE CONFIG FILE
+echo -e "\e[33m COPY EXPENSE CONFIG FILE\e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo CLEAN OLD CONTENT
+echo -e "\e[33mCLEAN OLD CONTENT\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo DOWNLOAD FRONTEND CONTENT
+echo -e "\e[33mDOWNLOAD FRONTEND CONTENT\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 
 
-echo EXTRACT CONTENT
+echo -e "\e[33m EXTRACT CONTENT\e[0m"
 unzip /tmp/frontend.zip
 
-echo RESTART SERVICE
+echo -e "\e[33m RESTART SERVICE\e[0m"
 systemctl enable nginx
 systemctl restart nginx
