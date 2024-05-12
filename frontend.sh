897435e7eb4fb1132ced7f63313d33585679aae1
Charan Reddy <kothacharanreddy@gmail.com>
2024-05-12 10:38:57 +0200

    Frontend Add Headings

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,19 @@
+echo INSTALLING NGINX
 dnf install nginx -y
 
-
+echo COPY EXPENSE CONFIG FILE
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-
+echo CLEAN OLD CONTENT
 rm -rf /usr/share/nginx/html/*
+
+echo DOWNLOAD FRONTEND CONTENT
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
+
+echo EXTRACT CONTENTunzip /tmp/frontend.zip
+
+echo RESTART SERVICE
 systemctl enable nginx
 systemctl restart nginx
